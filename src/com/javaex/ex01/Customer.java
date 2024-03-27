04ca41d5f89321381d4b1b75e1e7065132d298f4
하이미디어학원 <@DESKTOP-LJU20GE>
2024-03-27 15:35:00 +0900

    0327

---
+++ src/com/javaex/ex01/Customer.java
@@ -0,0 +1,12 @@
+package com.javaex.ex01;
+
+public class Customer extends Person{
+	private int	cNo;
+	private int point;
+	
+	public Customer(String name, String hp, int cNO, int point) {
+		
+	}
+
+	
+}
